51b15aa14739ed2150d61bac1a8ec26d1bb05b79
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-10-28 18:09:11 +0100

    variables

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,3 +1,20 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let myName = "Victor"
+let yourName = "Pedro"
+
+
+//MUTABLE
+var names = [myName, yourName]
+names.append("Bar")
+names.append("Baz")
+
+//INMUTABLE
+let nameInmu = [myName, yourName]
+
+//COPY DATA DONT AFFECT DE ORIGINAL DATA
+let moreName = ["Fran", "Santi"]
+var copy = moreName
+copy.append("VictorG")
+moreName
+copy
