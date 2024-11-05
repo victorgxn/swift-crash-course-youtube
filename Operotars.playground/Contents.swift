0c6ce00d166e21768525ce651bca066e8ffc8514
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-11-05 17:12:02 +0100

    operators

---
+++ Operotars.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+let myAge = 22
+let yourAge = 20
+
+
+if myAge > yourAge {
+     "I'm older than your"
+} else if myAge < yourAge {
+    "I'm younger than you"
+} else {
+    "Oh hey, we are the same age"
+}
+
+let myMothersAge = myAge + 30
+let doubleMyAge = myAge * 2
+
+ ///1. unary prefix
+let foo = !true
+///2. unary postfix
+let name = Optional("Vandad")
+type(of: name)
+let unaryPostFix = name!
+type(of: unaryPostFix)
+///3. binary infix
+let result = 1 + 2
+let names = "Foo" + " " + "Bar"
+
