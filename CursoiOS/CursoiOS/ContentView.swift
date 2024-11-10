a4e6ef752231a1d3d6ffedbaf061c29cf4fc6386
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-11-10 21:05:01 +0100

    SWIFT

---
+++ CursoiOS/CursoiOS/ContentView.swift
@@ -0,0 +1,24 @@
+ //
+//  ContentView.swift
+//  CursoiOS
+//
+//  Created by Victor Gonzalez Martin on 5/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CursoiOS/CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Victor Gonzalez Martin on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
