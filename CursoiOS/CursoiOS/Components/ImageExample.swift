88724403737da23812d0b56ac178862f9d10e83b
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-11-13 18:53:01 +0100

    componet image

--- CursoiOS/CursoiOS/Components/ImageExample.swift
+++ CursoiOS/CursoiOS/Components/ImageExample.swift
@@ -10,6 +10,13 @@ import SwiftUI
 struct ImageExample: View {
     var body: some View {
         Image("victorg")
+            .resizable()
+            .scaledToFill()
+            .frame(width:50, height: 90)
+        
+        Image(systemName: "figure.walk")
+            .resizable()
+            .frame(width: 100, height: 150)
     }
 }
 
