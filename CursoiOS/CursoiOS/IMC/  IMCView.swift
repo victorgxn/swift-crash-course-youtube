9a50af36575b2f3b8bf507cd7e948241a0287ff9
Victor Gonzalez <134617120+victorgxn@users.noreply.github.com>
2024-11-16 19:09:03 +0100

    componentes reutilizables parte 1

--- CursoiOS/CursoiOS/IMC/  IMCView.swift
+++ CursoiOS/CursoiOS/IMC/  IMCView.swift
@@ -8,10 +8,47 @@
 import SwiftUI
 
 struct IMCView: View {
+    
+    /*init() {
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
+    }*/
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                ToggleButton(text: "Victorg", imageName: "heart.fill", index: 0   )
+                ToggleButton(text: "Victorg", imageName: "heart.fill", index: 0   )
+            }
+        }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity).background(.backgroundApp)
+            //.navigationBarBackButtonHidden()
+            //.navigationTitle("IMC Calculator")
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("IMC Calculator").bold().foregroundColor(.white)
+                }
+            }
     }
 }
+
+struct ToggleButton:View {
+    
+    let text: String
+    let imageName: String
+    let index: Int
+    var body: some View {
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            VStack {
+                Image(systemName: imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                    .foregroundColor(.white)
+                Text(text)
+            }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity).background(.backgroundComponent)
+        })
+    }
+}
+
 
 #Preview {
     IMCView()
